cffe915be6179787e61c49056ed022bcced4d2fb
umb0021 <72857141+umb0021@users.noreply.github.com>
2021-02-20 09:06:35 +0100

    Add files via upload

---
+++ blog/navbar.py
@@ -0,0 +1,14 @@
+from . import nav
+from flask_nav.elements import *
+from flask_security import current_user
+
+@nav.navigation()
+def main_nav():
+    navbar = Navbar('Blog')
+    navbar.items.append(View('Home', 'main.index'))
+    if current_user.is_authenticated:
+        usergrp = []
+        usergrp.append(current_user.email)
+        usergrp.append(View('Logout', 'security.logout'))
+        navbar.items.append(Subgroup(*usergrp))
+    return navbar
